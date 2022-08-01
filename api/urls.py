edd18b79d8b58733f1ab46171fbb531d582fb5f9
dudleylim <dudleylim@gmail.com>
2022-08-01 11:40:58 +0800

    added create user functionality in views, urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('createUser/', views.createUser, name='create-user'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 # Create your views here.
+@api_view(['POST'])
+def createUser(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.create_user(username, '', password)
+        user.save()
+        return Response('user created')
